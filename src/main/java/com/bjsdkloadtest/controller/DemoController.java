91b11d51a2df441d3953db5e858948c0fee3fa8e
lsocha <lsocha@box.com>
2024-03-13 17:26:37 +0100

    add upload and download endpoints

--- src/main/java/com/bjsdkloadtest/controller/DemoController.java
+++ src/main/java/com/bjsdkloadtest/controller/DemoController.java
@@ -15,53 +15,60 @@ import java.util.UUID;
 @Controller
 public class DemoController {
 
-    private final Logger logger = LoggerFactory.getLogger(DemoController.class);
     private static final String JWT_CONFIG_PATH = "";
+    private final Logger logger = LoggerFactory.getLogger(DemoController.class);
     private final BoxDeveloperEditionAPIConnection api;
+    private final String testFileId;
+    private final String testFolderId;
 
     public DemoController() throws IOException {
         Reader reader = new FileReader(JWT_CONFIG_PATH);
         BoxConfig boxConfig = BoxConfig.readFrom(reader);
         this.api = BoxDeveloperEditionAPIConnection.getAppEnterpriseConnection(boxConfig);
+        BoxLogger.defaultLogger().setLevelToAll();
+
+        // Create test folder
+        BoxFolder parentFolder = new BoxFolder(api, "0");
+        BoxFolder.Info childFolderInfo = parentFolder.createFolder(UUID.randomUUID().toString());
+        logger.info(String.format("Created test folder %s with ID: %s", childFolderInfo.getName(), childFolderInfo.getID()));
+        this.testFolderId = childFolderInfo.getID();
+
+        // Upload a test file
+        this.testFileId = upload();
     }
 
-    @RequestMapping("/loadtest")
+
+    @RequestMapping("/download")
     @ResponseBody
-    public String runScenario() throws IOException {
-        BoxFolder parentFolder = new BoxFolder(api, "0");
+    public String download() throws IOException {
 
-        BoxFolder.Info childFolderInfo = parentFolder.createFolder(UUID.randomUUID().toString());
-        logger.info(String.format("Created folder %s with ID: %s", childFolderInfo.getName(), childFolderInfo.getID()));
-
-        BoxFolder folder = childFolderInfo.getResource();
-
-        for(int i=0; i<10; i++) {
-            FileInputStream stream;
-            try {
-                File resource = new ClassPathResource("file.pdf").getFile();
-                stream = new FileInputStream(resource.getPath());
-            } catch (FileNotFoundException e) {
-                continue;
-            }
-
-            BoxFile.Info file= folder.uploadFile(stream, UUID.randomUUID().toString());
-            logger.info(String.format("Uploaded file %s with ID: %s", file.getName(), file.getID()));
-        }
-
-        for (BoxItem.Info itemInfo : folder.getChildren()) {
-            BoxFile file = new BoxFile(api, itemInfo.getID());
-            OutputStream stream = new ByteArrayOutputStream();
-            file.download(stream);
-            stream.close();
-            logger.info(String.format("Downloaded file %s with ID: %s", itemInfo.getName(), itemInfo.getID()));
-
-            file.delete();
-            logger.info(String.format("Deleted file %s with ID: %s", itemInfo.getName(), itemInfo.getID()));
-        }
-
-        folder.delete(true);
-        logger.info(String.format("Deleted folder %s with ID: %s", childFolderInfo.getName(), childFolderInfo.getID()));
-
+        BoxFile file = new BoxFile(api, this.testFileId);
+        OutputStream stream = new ByteArrayOutputStream();
+        file.download(stream);
+        stream.close();
+        logger.info(String.format("Downloaded file with ID: %s", this.testFileId));
         return "SUCCESS";
     }
+
+
+    @RequestMapping("/upload")
+    @ResponseBody
+    public String upload() throws IOException {
+
+        BoxFolder folder = new BoxFolder(api, this.testFolderId);
+
+        FileInputStream stream;
+        try {
+            File resource = new ClassPathResource("file.pdf").getFile();
+            stream = new FileInputStream(resource.getPath());
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        BoxFile.Info fileInfo = folder.uploadFile(stream, UUID.randomUUID().toString());
+        logger.info(String.format("Uploaded file %s with ID: %s", fileInfo.getName(), fileInfo.getID()));
+        return fileInfo.getID();
+    }
+
 }
